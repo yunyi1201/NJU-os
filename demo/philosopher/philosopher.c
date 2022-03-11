ec943ed4688812ed7c7977d1123f5775f27010c3
yunyi <3050962819@qq.com>
2022-03-10 18:36:20 -0800

    add demo

--- demo/philosopher/philosopher.c
+++ demo/philosopher/philosopher.c
@@ -1,26 +1,38 @@
 #include "thread.h"
 #include "thread-sync.h"
 
+#include <assert.h>
+#include <stdbool.h>
+
 #define N 3
-sem_t locks[N];
+
+mutex_t lk = MUTEX_INIT();
+cond_t cv = COND_INIT();
+
+bool has_fork[N] = {false};
+
+void Tphilospher_eat(int id) {
+  printf("tphilosopher %d eat\n", id);
+}
 
 void Tphilosopher(int id) {
   int lhs = (N + id - 1) % N;
   int rhs = id % N;
-  while (1) {
-    P(&locks[lhs]);
-    printf("T%d Got %d\n", id, lhs + 1);
-    P(&locks[rhs]);
-    printf("T%d Got %d\n", id, rhs + 1);
-    V(&locks[lhs]);
-    V(&locks[rhs]);
+  mutex_lock(&lk);
+  while (!has_fork[lhs] && has_fork[rhs]) {
+    cond_wait(&cv, &lk);
   }
+  Tphilospher_eat(id);
+  has_fork[lhs] = has_fork[rhs] = false;
+  mutex_unlock(&lk);   
+
+  mutex_lock(&lk);
+  has_fork[lhs] = has_fork[rhs] = true;
+  cond_broadcast(&cv);
+  mutex_unlock(&lk);
 }
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < N; i++) {
-      SEM_INIT(&locks[i], 1);
-  }
   for (int i = 0; i < N; i++) {
     create(Tphilosopher);
   }
